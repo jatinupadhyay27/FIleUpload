f73aa846fd15ecb7089b0dd5857ce31c616d14b5
jatinupadhyay27 <167077610+jatinupadhyay27@users.noreply.github.com>
2024-07-27 01:10:02 +0530

    header issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl text-center mt-5 font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent ">
+        <h1 className="text-3xl text-center font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent my-header" style={{marginTop: "50px"}}>
           File Upload And Download
         </h1>
       </header>
